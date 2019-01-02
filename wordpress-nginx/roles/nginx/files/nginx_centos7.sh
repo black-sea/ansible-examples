ec46251ca32a37769a22f1ba09eccbf5ff7e6df4
root <root@localhost.localdomain>
2019-01-02 10:23:35 +0800

    新增lnmp+wordes架构例子

---
+++ wordpress-nginx/roles/nginx/files/nginx_centos7.sh
@@ -0,0 +1,16 @@
+[Unit]
+Description=nginx - high performance web server
+Documentation=http://nginx.org/en/docs/
+After=network.target remote-fs.target nss-lookup.target
+
+[Service]
+Type=forking
+WorkingDirectory=/usr/local/nginx
+ExecStart=/usr/local/nginx/sbin/nginx
+ExecReload=/bin/kill -s HUP $MAINPID
+ExecStop=/bin/kill -s QUIT $MAINPID
+PrivateTmp=true
+
+
+[Install]
+WantedBy=multi-user.target
