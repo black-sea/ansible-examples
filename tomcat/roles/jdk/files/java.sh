7067faad6b2375ed2a9f8fc1b7c8f2ab8edaa468
linsir <linsir18@gmail.com>
2019-01-03 18:40:09 +0800

    新增部署tomcat

---
+++ tomcat/roles/jdk/files/java.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME=/usr/local/jdk1.8.0_151
+export PATH=$JAVA_HOME/bin:$PATH
+export JRE_HOME=/usr/local/jdk1.8.0_151
+export CLASSPATH=/usr/local/jdk1.8.0_151/lib/tools.jar:/usr/local/jdk1.8.0_151/lib/dt.jar
---
+++ tomcat/roles/tomcat/files/tomcat_centos7.sh
@@ -0,0 +1,16 @@
+[Unit]
+Description=Tomcat
+After=syslog.target network.target remote-fs.target nss-lookup.target
+
+[Service]
+Type=forking
+Evironment="JAVA_HOME=/usr/local/jdk1.8.0_115"
+PIDFile=/usr/local/apache-tomcat/tomcat.pid
+ExecStart=/usr/local/apache-tomcat/bin/startup.sh
+ExecReload=/bin/kill -s HUP $MAINPID
+ExecStop=/bin/kill -s QUIT $MAINPID
+PrivateTmp=true
+
+[Install]
+WantedBy=multi-user.target
+
